ec3895fd1575082c25f4f32caa0d10f21b42bb11
Sebastian Guzman <sebastiangf2106@gmail.com>
2026-02-11 10:03:01 -0500

    Initial commit

---
+++ Controllers/ValueController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace simpletest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+    }
+}
